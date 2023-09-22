5843c203fd2bc7aa8b3d3b79bf566ebd287d0596
JoaoSilva <up201906478@up.pt>
2023-09-22 10:24:15 +0100

    Add commands to tournament instances

--- SampleSolutionBowling/org.eclipse.example.bowling.tutorial/src/org/eclipse/example/bowling/tutorial/popup/actions/ExampleLeagueDialog.java
+++ SampleSolutionBowling/org.eclipse.example.bowling.tutorial/src/org/eclipse/example/bowling/tutorial/popup/actions/ExampleLeagueDialog.java
@@ -5,7 +5,7 @@ import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
 import org.eclipse.emf.databinding.EMFObservables;
 import org.eclipse.jface.databinding.swt.ISWTObservableValue;
-import org.eclipse.jface.databinding.swt.SWTObservables;
+import org.eclipse.jface.databinding.swt.typed.WidgetProperties;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
@@ -17,32 +17,30 @@ import bowling.Player;
 
 public class ExampleLeagueDialog extends AbstractLeagueExampleDialog {
 
-	private Text text;
-	private DataBindingContext context;
-	private Binding bindValue;
+    private Text text;
+    private DataBindingContext context;
+    private Binding bindValue;
 
-	protected ExampleLeagueDialog(Shell parentShell) {
-		super(parentShell);
-	}
+    protected ExampleLeagueDialog(Shell parentShell) {
+        super(parentShell);
+    }
 
-	@Override
-	protected void createNameComposite(Composite nameComposite) {
-		//Create a label, a text field and a databinding context
-	}
+    @Override
+    protected void createNameComposite(Composite nameComposite) {
+        Label label = new Label(nameComposite, SWT.NONE);
+        label.setText("Name: ");
+        text = new Text(nameComposite, SWT.NONE);
+        context = new DataBindingContext();
+    }
 
-	@Override
-	protected void setActivePlayer(Player firstElement) {
-		//Remove old binding if existing and bind text field to name attribute
-	}
-
-
-	
-
-	
-
-
-
-
-	
-
+    @Override
+    protected void setActivePlayer(Player firstElement) {
+        if(bindValue!=null){
+            bindValue.dispose();
+        }
+        text.setText("");
+        ISWTObservableValue observeText = WidgetProperties.text(SWT.Modify).observe(text);
+        IObservableValue observeValue = EMFObservables.observeValue(firstElement, BowlingPackage.eINSTANCE.getPlayer_Name());
+        bindValue = context.bindValue(observeText, observeValue);
+    }
 }
--- SampleSolutionBowling/org.eclipse.example.bowling.tutorial/src/org/eclipse/example/bowling/tutorial/popup/actions/ExampleLeagueDialogSolution.java
+++ SampleSolutionBowling/org.eclipse.example.bowling.tutorial/src/org/eclipse/example/bowling/tutorial/popup/actions/ExampleLeagueDialogSolution.java
@@ -5,7 +5,7 @@ import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
 import org.eclipse.emf.databinding.EMFObservables;
 import org.eclipse.jface.databinding.swt.ISWTObservableValue;
-import org.eclipse.jface.databinding.swt.SWTObservables;
+import org.eclipse.jface.databinding.swt.typed.WidgetProperties;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
@@ -17,41 +17,30 @@ import bowling.Player;
 
 public class ExampleLeagueDialogSolution extends AbstractLeagueExampleDialog {
 
-	private Text text;
-	private DataBindingContext context;
-	private Binding bindValue;
+    private Text text;
+    private DataBindingContext context;
+    private Binding bindValue;
 
-	protected ExampleLeagueDialogSolution(Shell parentShell) {
-		super(parentShell);
-	}
+    protected ExampleLeagueDialogSolution(Shell parentShell) {
+        super(parentShell);
+    }
 
-	@Override
-	protected void createNameComposite(Composite nameComposite) {
-		Label label = new Label(nameComposite, SWT.NONE);
-		label.setText("Name: ");
-		text = new Text(nameComposite, SWT.NONE);
-		context = new DataBindingContext();
-	}
+    @Override
+    protected void createNameComposite(Composite nameComposite) {
+        Label label = new Label(nameComposite, SWT.NONE);
+        label.setText("Name: ");
+        text = new Text(nameComposite, SWT.NONE);
+        context = new DataBindingContext();
+    }
 
-	@Override
-	protected void setActivePlayer(Player firstElement) {
-		if(bindValue!=null){
-			bindValue.dispose();
-		}
-		text.setText("");
-		ISWTObservableValue observeText = SWTObservables.observeText(text, SWT.Modify);
-		IObservableValue observeValue = EMFObservables.observeValue(firstElement, BowlingPackage.eINSTANCE.getPlayer_Name());
-		bindValue = context.bindValue(observeText, observeValue);
-	}
-
-
-	
-
-	
-
-
-
-
-	
-
+    @Override
+    protected void setActivePlayer(Player firstElement) {
+        if(bindValue!=null){
+            bindValue.dispose();
+        }
+        text.setText("");
+        ISWTObservableValue observeText = WidgetProperties.text(SWT.Modify).observe(text);
+        IObservableValue observeValue = EMFObservables.observeValue(firstElement, BowlingPackage.eINSTANCE.getPlayer_Name());
+        bindValue = context.bindValue(observeText, observeValue);
+    }
 }
